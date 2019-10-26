6d98248b55297ce7a6c606f3cccc5ef0baf21195
Amir Honarpour <amir@honarpour.com>
2019-10-26 12:28:48 -0400

    feat(global): Add styled components and refactor app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+import GlobalStyles from './GlobalStyles';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <GlobalStyles />
+      <Wrapper>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </Wrapper>
+    </>
   );
-}
+};
+
+const Wrapper = styled.div`
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+`;
 
 export default App;
